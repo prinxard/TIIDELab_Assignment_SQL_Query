79415e6931537f625f4774f859a85dea73105ff3
prinxard <prinxard@gmail.com>
2020-04-03 07:39:33 +0100

    Add files via upload

--- SQL_query.sql
+++ SQL_query.sql
@@ -1,9 +1,5 @@
-ALTER TABLE customers
-ADD COLUMN imigration_status varchar(255);
-
-UPDATE customers 
-SET 
-    address = '25 Sage Terrace'
-WHERE
-    customer_id = 1;
-
+SELECT * FROM order_items
+WHERE order_id = 6
+AND unit_price * quantity > 30;
+SELECT * FROM products
+WHERE quantity_in_stock IN (38, 72, 49);
